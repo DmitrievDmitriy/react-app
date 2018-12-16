6dd5348f45020276b229ccc0b058951a5051981b
Dmitriy <17sokol@i.ua>
2018-12-16 16:13:57 +0200

    Updates

---
+++ precache-manifest.56c594a40f59335e6c1e749a47114e37.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ec46125784e0b1f6cca6",
+    "url": "/react-app/static/js/runtime~main.ec461257.js"
+  },
+  {
+    "revision": "50a4a4396ba7749071d2",
+    "url": "/react-app/static/js/main.50a4a439.chunk.js"
+  },
+  {
+    "revision": "cafccc146146d1fa565f",
+    "url": "/react-app/static/js/1.cafccc14.chunk.js"
+  },
+  {
+    "revision": "50a4a4396ba7749071d2",
+    "url": "/react-app/static/css/main.2f122410.chunk.css"
+  },
+  {
+    "revision": "c55cee140ca5b9ff8c5f23cda3384f0c",
+    "url": "/react-app/index.html"
+  }
+];
